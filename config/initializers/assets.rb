acd6b05cb8efe58b0b1dc9ef09f6c7dca16e941c
Andy Hardy <aohardy@gmail.com>
2025-11-28 07:50:05 -0500

    update plotly import

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,9 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Add plotly-rails-js gem assets to the load path
+plotly_gem_path = Bundler.rubygems.find_name("plotly-rails-js").first&.full_gem_path
+if plotly_gem_path
+  Rails.application.config.assets.paths << File.join(plotly_gem_path, "vendor", "assets", "javascripts")
+end
