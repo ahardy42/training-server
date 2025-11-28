15b27c4a54a17474b46f403095a1631737959624
Andy Hardy <aohardy@gmail.com>
2025-11-28 07:20:43 -0500

    add conversions based on user's units in app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,9 +16,67 @@ module ApplicationHelper
   def format_pace(minutes_per_km)
     return "0:00" if minutes_per_km.nil? || minutes_per_km.zero?
 
-    minutes = minutes_per_km.to_i
-    seconds = ((minutes_per_km - minutes) * 60).to_i
+    # Convert to minutes per mile if user prefers imperial
+    pace = user_prefers_imperial? ? minutes_per_km * 1.60934 : minutes_per_km
+
+    minutes = pace.to_i
+    seconds = ((pace - minutes) * 60).to_i
 
     format("%d:%02d", minutes, seconds)
+  end
+
+  def format_distance(distance_km)
+    return nil if distance_km.nil?
+
+    if user_prefers_imperial?
+      distance_miles = distance_km * 0.621371
+      number_with_precision(distance_miles, precision: 2)
+    else
+      number_with_precision(distance_km, precision: 2)
+    end
+  end
+
+  def distance_unit
+    user_prefers_imperial? ? "mi" : "km"
+  end
+
+  def format_elevation(elevation_m)
+    return nil if elevation_m.nil?
+
+    if user_prefers_imperial?
+      elevation_ft = elevation_m * 3.28084
+      number_with_precision(elevation_ft, precision: 0)
+    else
+      number_with_precision(elevation_m, precision: 0)
+    end
+  end
+
+  def elevation_unit
+    user_prefers_imperial? ? "ft" : "m"
+  end
+
+  def pace_unit
+    user_prefers_imperial? ? "/mi" : "/km"
+  end
+
+  def format_speed(kmh)
+    return nil if kmh.nil?
+
+    if user_prefers_imperial?
+      mph = kmh * 0.621371
+      number_with_precision(mph, precision: 2)
+    else
+      number_with_precision(kmh, precision: 2)
+    end
+  end
+
+  def speed_unit
+    user_prefers_imperial? ? "mph" : "km/h"
+  end
+
+  private
+
+  def user_prefers_imperial?
+    current_user&.units == "imperial"
   end
 end
